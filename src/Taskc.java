4fd0aae723559cd55ecc3acba85b215659acdc1a
Student <student@gmail.com>
2026-09-24 16:13:30 +0600

    third

---
+++ src/Taskc.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Taskc {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int k = input.nextInt();
+        int a = k/n;
+        System.out.println(a);
+
+    }
+}
